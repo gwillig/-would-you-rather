379d18e8cd0dae41591778cfb949b17bb0656543
gwillig <gustavwillig@gmail.com>
2020-11-04 20:37:10 +0000

    MOD|Add UserCard for Leader_board.js

--- src/components/Leader_Board/Leader_board.js
+++ src/components/Leader_Board/Leader_board.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import {Jumbotron } from "react-bootstrap";
+import UserCard from "./UserCard";
 
 class Leader_board extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {  user:[{name:"gustav"},{name:"tom"},{name:"hans"}]}
+        this.state = {
+            user:[
+                {name:"gustav",sum_question:3,sum_answer:3,score:6},
+                {name:"hans",sum_question:3,sum_answer:3,score:6},
+                {name:"tom",sum_question:3,sum_answer:3,score:6}
+                ]
+        }
     }
     render() {
         return (
             <Jumbotron>
-                {this.state.user.map((user)=>{return <h1>{user.name}</h1>})}
+                {this.state.user.map((user)=>{return (
+                    <UserCard
+                        user_name={user.name}
+                        sum_question={user.sum_question}
+                        sum_answer={user.sum_answer}
+                        score={user.score}
+                    >
 
+                    </UserCard>
 
+                )})}
             </Jumbotron>
         )
     }
---
+++ src/components/Leader_Board/UserCard.js
@@ -0,0 +1,42 @@
+import {Col, Container, Row} from "react-bootstrap";
+import avatar_johndoe from "../image/johndoe.png";
+import React from "react";
+
+
+class UserCard extends React.Component{
+
+    style_card={
+        backgroundColor: "#fff",
+        backgroundClip: "border-box",
+        border:" 1px solid #19647e",
+        borderRadius: "50px",
+        padding: "30px",
+        margin:"10px"
+
+    }
+    avatar_style={
+        width:"100px"
+    }
+
+    render(){
+        return(
+            <Container>
+            <Row id={this.props.id}style={this.style_card} >
+                <Col xs lg="3" style={this.col_styl}>
+                    <img style={this.avatar_style} src={avatar_johndoe}></img>
+                </Col>
+                <Col style={this.col_styl}>
+                    <h4>{this.props.name}</h4>
+                    <h5>Answered questions: {this.props.sum_answer}</h5>
+                    <h5>Created questions: {this.props.sum_question}</h5>
+                    <h3>Score: {this.props.score}</h3>
+
+                </Col>
+            </Row>
+            </Container>
+        )
+    }
+
+}
+
+export default UserCard
